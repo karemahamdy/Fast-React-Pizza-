56942fe4db58780ba8cc80d76a9ef47038e42d2d
Karema <karemahamdy51@gmail.com>
2024-10-16 04:26:15 +0300

    build add tailwind to this project

---
+++ src/ui/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+    <header className="flex justify-between h-16 bg-yellow-400 items-center border-b-2 border-gray-100 px-4 py-3 uppercase sm:px-6">
+     <h1 className="tracking-widest">Fast   React   Pizza   Co.</h1>
+    </header>
+    </>
+  )
+}
+export default Header
