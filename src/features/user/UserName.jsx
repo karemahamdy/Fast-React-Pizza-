f9f3e41d5c80005e65da5cb9a2ca7a816d438a91
Karema <karemahamdy51@gmail.com>
2024-10-18 00:17:34 +0300

    style: heaer page style

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,10 @@
+
+
+function User() {
+  return (
+    <>
+     <h1 className="font-semibold ">karema</h1>
+    </>
+  )
+}
+export default User
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom"
 import SearchOrder from "../features/order/SearchOrder"
+import User from "../features/user/UserName"
 function Header() {
   return (
     <>
-    <header className="flex justify-between h-16 bg-yellow-400 items-center border-b-2 border-gray-100 px-4 py-3 uppercase sm:px-6">
+    <header className="flex  justify-between h-16 bg-yellow-400 items-center border-b-2 border-gray-100 px-4 py-3 uppercase sm:px-6">
     <Link to="/">
      <h1 className="tracking-widest">Fast   React   Pizza   Co.</h1>
-     <SearchOrder/>
     </Link>
+
+     <SearchOrder/>
+     <User/>
+     
     </header>
     </>
   )
