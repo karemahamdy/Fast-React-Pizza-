dd5a6d64048c8a78a9315c1772d6be452a72fa76
Karema <karemahamdy51@gmail.com>
2024-10-18 22:05:42 +0300

    fix: err of navigate button

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import  Button  from "./Button"
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className="my-10 px-4 text-center sm:my-16 ">
       
@@ -9,7 +13,11 @@ function Home() {
         <br />
       <span className="text-yellow-500 text-[32px] tracking-widest">Straight out of the oven, straight to you.</span>  
       </h1>
+      {username === '' ? (
       <CreateUser/>
+      ): (
+        <Button type="primary" to = "/menu" >Continue ordering {username} </Button>
+      )}
     </div>
   );
 }
